38f350f8fe4fdb27e673ad11ff4996b9ac7f2c8e
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-12-12 07:49:18 +0100

    Core restructured

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,8 +1,5 @@
 ﻿// SPDX-License-Identifier: MIT
 // Copyright (c) 2025. Csaba Dudas (CsabaDu)
 
-global using CsabaDu.DynamicTestData.Core.Statics;
-global using CsabaDu.DynamicTestData.Core.TestDataTypes;
+global using CsabaDu.DynamicTestData.Core.DataStrategyTypes;
 global using CsabaDu.DynamicTestData.Lite.DynamicDataSources;
-global using System.ComponentModel;
-global using System.Reflection;
